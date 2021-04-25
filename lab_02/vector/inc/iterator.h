29737bee197deaf5107eae3c6cdb0aa2bc5939dc
mmd19u540 <mymidi.student@yandex.ru>
2021-04-25 23:15:44 +0300

    ADD: iterator

--- lab_02/vector/inc/iterator.h
+++ lab_02/vector/inc/iterator.h
@@ -3,14 +3,46 @@
 
 #include <memory>
 
+using namespace std;
+
+template <typename Type> class Vector;
+
 template <typename Type>
-class Iterator : public std::iterator<std::random_access_iterator_tag, Type>
+class Iterator : public iterator<input_iterator_tag, Type>
 {
+    friend class Vector<Type>;
+
 public:
-    Iterator();
+    Iterator(const Vector<Type> &vector) noexcept;
+    Iterator(const Iterator<Type> &iterator) noexcept = default;
+
+    // перепроверить конструкторы !!!
+    ~Iterator() = default;
+
+    Type & operator*();
+    Type * operator->();
+
+    const Type & operator*() const;
+    const Type * operator->() const;
+
+    Iterator<Type> operator+(const size_t dif) const;
+
+    Iterator<Type> & operator++() noexcept;
+    Iterator<Type> operator++(int) noexcept;
+
+    bool operator==(const Iterator<Type> &iterator) const;
+    bool operator!=(const Iterator<Type> &iterator) const;
+
+protected:
+    void exprideCheck(const uint line);
+    void indexCheck(const uint line);
 
 private:
-    std::weak_ptr<Type[]> ptr;
+    weak_ptr<Type[]> ptr;
+    size_t size = 0;
+    size_t index = 0;
 };
+
+#include "iterator.hpp"
 
 #endif // ITERATOR_H
--- lab_02/vector/src/iterator.hpp
+++ lab_02/vector/src/iterator.hpp
@@ -1,6 +1,106 @@
-#include "../inc/iterator.h"
+#include <time.h>
+#include "iterator.h"
+#include "exceptions.h"
 
-Iterator::Iterator()
+template <typename Type>
+Iterator<Type>::Iterator(const Vector<Type> &vector) noexcept
 {
+    ptr = vector.data;
+    size = vector.size;
+    index = 0;
+}
 
+template <typename Type>
+Type &Iterator<Type>::operator*()
+{
+    exprideCheck(__LINE__);
+    indexCheck(__LINE__);
+
+    return ptr.lock()[index];
+}
+
+template <typename Type>
+const Type &Iterator<Type>::operator*() const
+{
+    exprideCheck(__LINE__);
+    indexCheck(__LINE__);
+
+    // Возможно, здесь нужна локальная переменная
+    return ptr.lock()[index];
+}
+
+template <typename Type>
+Type *Iterator<Type>::operator->()
+{
+    exprideCheck(__LINE__);
+    indexCheck(__LINE__);
+
+    return ptr.lock() + index;
+}
+
+template <typename Type>
+const Type *Iterator<Type>::operator->() const
+{
+    exprideCheck(__LINE__);
+    indexCheck(__LINE__);
+
+    return ptr.lock() + index;
+}
+
+template <typename Type>
+Iterator<Type> & Iterator<Type>::operator++() noexcept
+{
+    ++index;
+    return *this;
+}
+
+template <typename Type>
+Iterator<Type> Iterator<Type>::operator++(int) noexcept
+{
+    Iterator<Type> tmp(*this);
+    ++(*this);
+    return tmp;
+}
+
+template <typename Type>
+Iterator<Type> Iterator<Type>::operator+(const size_t dif) const
+{
+    // Ошибки?
+    Iterator<Type> tmp(*this);
+    tmp.index += dif;
+    return tmp;
+}
+
+template <typename Type>
+bool Iterator<Type>::operator==(const Iterator<Type> &iterator) const
+{
+    return ptr.lock() == iterator.ptr.lock() && index == iterator.index;
+}
+
+template <typename Type>
+bool Iterator<Type>::operator!=(const Iterator<Type> &iterator) const
+{
+    return !(*this == iterator);
+}
+
+template <typename Type>
+void Iterator<Type>::exprideCheck(const uint line)
+{
+    if (ptr.expired())
+    {
+        time_t curTime = time(NULL);
+        throw ExpiredException(ctime(&curTime), __FILE__, line,
+                               typeid(*this).name(), __FUNCTION__);
+    }
+}
+
+template <typename Type>
+void Iterator<Type>::indexCheck(const uint line)
+{
+    if (index >= size)
+    {
+        time_t curTime = time(NULL);
+        throw OutOfRangeException(ctime(&curTime), __FILE__, line,
+                               typeid(*this).name(), __FUNCTION__);
+    }
 }
